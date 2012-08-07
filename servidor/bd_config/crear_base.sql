0c02b1692eb6bcf87b0ed42a5a5aa10ad37d50e2
Cid Reyes <cid.reyes@lasquinceletras.com.mx>
2012-08-07 16:20:37 -0500

    Archivos de configuración de la base de datos.

---
+++ servidor/bd_config/crear_base.sql
@@ -0,0 +1,6 @@
+-- Script para crear la base en MySQL
+
+create database Evaluador
+       character set 'utf8';
+
+
---
+++ servidor/bd_config/crear_tablas.sql
@@ -0,0 +1,19 @@
+-- Crear tablas para la base de SQL
+use evaluador;
+
+create table usuarios (
+       IDusuario      int		NOT NULL	AUTO_INCREMENT,
+       clave	      varchar(10)	NOT NULL,
+       nombre	      varchar(100)	NOT NULL,
+       pass	      text		NOT NULL,
+       primary key(IDusuario));
+
+create table entregas (
+       IDentrega 	      int		NOT NULL,
+       IDusuario	      int		NOT NULL,
+       tarea		      int		NOT NULL,
+       ejercicio	      int		NOT NULL,
+       objeto		      text		NOT NULL,
+       evaluado		      boolean		NOT NULL,
+       calificacion	      int		NOT NULL,
+       primary key(IDentrega));
