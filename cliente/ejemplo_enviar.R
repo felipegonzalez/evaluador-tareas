4827180746ebd0af2fdf365e6c1bcc4353fda35b
Cid Reyes <cid.reyes@lasquinceletras.com.mx>
2012-08-22 13:02:27 -0500

    Modificar el cliente.

--- cliente/ejemplo_enviar.R
+++ cliente/ejemplo_enviar.R
@@ -3,9 +3,9 @@ library(RCurl)
 ## Este es un prototipo de cómo debe funcionar el cliente
 
 enviar.fun <- function(nombre, my_key, funcion.archivo, tarea, ejercicio, nombre.fun){
-   #serv.1 <- 'http://ec2-23-22-24-116.compute-1.amazonaws.com/r/'
+   servidor <- 'http://ec2-23-22-6-197.compute-1.amazonaws.com/evaluador'
    #servidor.1 <- paste(serv.1, 'aceptar.R', sep='')
-   servidor <- 'http://localhost:4567/evaluador' #para pruebas locales
+   #servidor <- 'http://localhost:4567/evaluador' #para pruebas locales
    con <- file(funcion.archivo, "r", blocking = TRUE)
    #source(funcion.archivo)
    texto.fun <- as.character(base64Encode(rawToChar(serialize(readLines(con), ascii=TRUE, connection=NULL))))
@@ -15,7 +15,7 @@ enviar.fun <- function(nombre, my_key, funcion.archivo, tarea, ejercicio, nombre
    print("ok")
    #print(texto.fun)
    curl = getCurlHandle()
-   curlSetOpt(.opts = list(userpwd = "admin:admin"),
+   curlSetOpt(.opts = list(userpwd = paste(nombre,my_key,sep=":")),
               curl = curl)
    # autenticacion
    html <- postForm(servidor, nombre = nombre, pass=my_key, tarea=tarea,ejercicio=ejercicio, funcion=texto.fun, style="httppost",
@@ -33,15 +33,15 @@ enviar <- function(){
    cat("Clave:")
    my_key <- scan(what=character(), n=1, quiet =TRUE)
    cat("Cuál quieres enviar: \n")
-   cat("1 post_binomial.R  \n")
-   cat("2 iterar_metro.R \n")
+   cat("tarea_prueba.R  \n")
    parte <- scan(what=double(), n = 1, quiet=TRUE)
-    
    if(as.integer(parte)==1){
-      enviar.fun(nombre=nombre, my_key=my_key, funcion.archivo='tarea_prueba.R', tarea="1",ejercicio="1", nombre.fun='ejemplo')
-   }
-   if(as.integer(parte)==2){
-      enviar.fun(nombre=nombre, my_key=my_key, funcion.archivo='iterar_metro.R', tarea="1",ejercicio="2")
+     enviar.fun(nombre=nombre,
+                my_key=my_key,
+                funcion.archivo='tarea_prueba.R',
+                tarea="1",
+                ejercicio="1",
+                nombre.fun='ejemplo')
    }
 }
 
